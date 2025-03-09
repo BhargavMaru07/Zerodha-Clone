6eb20b97c7c0f94362484053c2afc4445b16e94e
BhargavMaru07 <bhargavhamir@gmail.com>
2025-03-09 22:48:08 +0530

    setup

---
+++ frontend/src/landing_page/home/HomePage.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Hero from './Hero'
+import Awards from './Awards'
+import Pricing from './Pricing'
+import Education from './Education'
+import Stats from './Stats'
+
+const HomePage = () => {
+  return (
+    <>
+    <Hero/>
+    <Awards/>
+    <Pricing/>
+    <Education/>
+    <Stats/>
+    </>
+
+  )
+}
+
+export default HomePage
