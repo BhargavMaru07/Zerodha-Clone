4e2dcdd280a7dbb1180960cf5499c2ab2a9fa118
BhargavMaru07 <bhargavhamir@gmail.com>
2025-04-30 22:57:27 +0530

    change

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const {holdings} = require('../dashboard/src/data/data')
+const {positions} = require('../dashboard/src/data/data')
+const Holding = require('./model/HoldingsModel')
+const Position = require("./model/PositionsModel");
 
 const PORT = process.env.PORT || 3333;
 const URL = process.env.MONGO_URL;
@@ -15,6 +19,31 @@ mongoose
   .catch((e) => {
     console.log("Error in database",e);
   });
+
+
+// //dumy data route
+// //holding
+// app.get('/addHoldings',async(req,res)=>{
+//   try {
+//     await Holding.insertMany(holdings)
+//     console.log("holding inserted");
+//     res.send("done")
+//   } catch (error) {
+//     console.error('error in insertion',error)
+//   }
+// })
+
+// //position
+// app.get('/addPositions',async(req,res)=>{
+//   try {
+//     await Position.insertMany(positions);
+//     console.log("positions inserted");
+//     res.send("done")
+//   } catch (error) {
+//     console.error('error in insertion',error)
+//   }
+// })
+
 
 app.listen(PORT, () => {
   console.log(`server is listining on ${PORT}`);
--- backend/model/PositionsModel.js
+++ backend/model/PositionsModel.js
@@ -1,6 +1,6 @@
 const { model } = require("mongoose");
 const { PositionsSchema } = require("../schemas/PositionsSchema");
 
-const Position = model("Position", HoldingSchema);
+const Position = model("Position", PositionsSchema);
 
 module.exports = Position;
